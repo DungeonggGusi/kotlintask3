5d766d4eeda9431b9dfec22905058117811d3ae2
DungeonggGusi <krivtsovyaroslav@gmail.com>
2025-06-20 21:21:36 +0300

    Create DatabaseConfig.kt

---
+++ config/DatabaseConfig.kt
@@ -0,0 +1,15 @@
+package ru.dungeongg.kotlintask3.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcTemplate
+import javax.sql.DataSource
+
+@Configuration
+class DatabaseConfig {
+
+    @Bean
+    fun jdbcTemplate(dataSource: DataSource): JdbcTemplate {
+        return JdbcTemplate(dataSource)
+    }
+}
